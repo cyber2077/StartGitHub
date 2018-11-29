14adf577c68f995aaba41628bd4411fec83b189a
hong <hong@WIN-HNEBFLIAN68>
2018-11-29 23:13:36 +0900

    コミット　プッシュ　2回目

---
+++ src/test000001.java
@@ -0,0 +1,10 @@
+class test000001 {
+    public static void main(String[] args) {
+	int aa = 10;
+	for (int i = aa; i >= 1; i--) {
+	    System.out.println(i);
+
+	    // test用の sample
+	}
+    }
+}
